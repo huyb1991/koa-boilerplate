c47be6a8ac92ff9d22a2f625268666dc2f2b06d9
Huy Nguyen <huyb.1991@gmail.com>
2018-06-26 11:23:16 +0700

    Add whitelist config and CORS options

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,3 +1,13 @@
 module.exports = {
-  DB_URL: process.env.DB_URL
+  DB_URL: process.env.DB_URL,
+  CORS: {
+    origin: ctx => {
+      const requestOrigin = ctx.accept.headers.origin;
+
+      if (!process.env.WHITELIST.includes(requestOrigin)) {
+        return new Error(`${requestOrigin} is not allowed by CORS`);
+      }
+      return requestOrigin;
+    }
+  }
 }
